d2e43f1ad9226f914e2c92f6558648a9b3fee941
maeda-takumi <jiangqiantian54@gmail.com>
2025-12-29 16:02:04 +0900

    11

--- config/youtube.php
+++ config/youtube.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_key' => getenv('YOUTUBE_API_KEY') ?: '',
+    'api_key' => 'AIzaSyBuxDiA49t6B9fEPq8VgW5-FOz3Z8xtnnk',
     'base_url' => 'https://www.googleapis.com/youtube/v3',
     'max_batch_size' => 45, // Safer than the API limit of 50 to avoid URL length issues
 ];
